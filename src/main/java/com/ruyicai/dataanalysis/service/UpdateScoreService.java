7a11ce6cf6040c5333ac19f45cb2f76fc96c38a1
qujinlong <qujinlong@ruyicai.com>
2013-06-28 18:11:33 +0800

    竞彩足球比分更新的定时任务：更新开赛时间和上下半场的开始时间，用于前端显示比赛进行了多长时间

--- src/main/java/com/ruyicai/dataanalysis/service/UpdateScoreService.java
+++ src/main/java/com/ruyicai/dataanalysis/service/UpdateScoreService.java
@@ -1,5 +1,10 @@
 package com.ruyicai.dataanalysis.service;
 
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.commons.lang.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +13,7 @@ import org.springframework.stereotype.Service;
 
 import com.ruyicai.dataanalysis.consts.MatchState;
 import com.ruyicai.dataanalysis.domain.Schedule;
+import com.ruyicai.dataanalysis.util.DateUtil;
 import com.ruyicai.dataanalysis.util.HttpUtil;
 import com.ruyicai.dataanalysis.util.NumberUtil;
 import com.ruyicai.dataanalysis.util.StringUtil;
@@ -53,6 +59,8 @@ public class UpdateScoreService {
 			data = data.replaceAll("\"", "");
 			String[] values = data.split("\\^");
 			String scheduleID = values[0];
+			String matchTime = values[11];
+			String matchTime2 = values[12];
 			String matchState = values[13];
 			String homeScore = values[14];
 			String guestScore = values[15];
@@ -68,7 +76,7 @@ public class UpdateScoreService {
 			if(null == schedule) {
 				return;
 			}
-			int oldMatchState = schedule.getMatchState();
+			int oldMatchState = schedule.getMatchState(); //比赛状态
 			boolean ismod = false;
 			if(Integer.parseInt(matchState) != schedule.getMatchState()) {
 				ismod = true;
@@ -114,6 +122,24 @@ public class UpdateScoreService {
 				ismod = true;
 				schedule.setGuest_Order(guestOrder);
 			}
+			//解析开赛时间
+			Map<String, String> matchTimeMap = getMatchTimeMap(matchTime, matchTime2);
+			matchTime = matchTimeMap.get("matchTime");
+			matchTime2 = matchTimeMap.get("matchTime2");
+			//开赛时间
+			Date oldMatchTime = schedule.getMatchTime();
+			if (StringUtils.isNotBlank(matchTime)&&(oldMatchTime==null
+					||!StringUtils.equals(matchTime, DateUtil.format("yyyy-MM-dd HH:mm:ss", oldMatchTime)))) {
+				ismod = true;
+				schedule.setMatchTime(DateUtil.parse("yyyy-MM-dd HH:mm:ss", matchTime));
+			}
+			//上下半场开始时间
+			Date oldMatchTime2 = schedule.getMatchTime2();
+			if (StringUtils.isNotBlank(matchTime2)&&(oldMatchTime2==null
+					||!StringUtils.equals(matchTime2, DateUtil.format("yyyy-MM-dd HH:mm:ss", oldMatchTime2)))) {
+				ismod = true;
+				schedule.setMatchTime2(DateUtil.parse("yyyy-MM-dd HH:mm:ss", matchTime2));
+			}
 			if(ismod) {
 				schedule.merge();
 				if(MatchState.WANCHANG.value != oldMatchState && MatchState.WANCHANG.value == schedule.getMatchState()) {
@@ -124,4 +150,37 @@ public class UpdateScoreService {
 			logger.error(e.getMessage(), e);
 		}
 	}
+	
+	/**
+	 * 解析比赛时间
+	 * @param matchTime
+	 * @param matchTime2
+	 * @return
+	 */
+	private Map<String, String> getMatchTimeMap(String matchTime, String matchTime2) {
+		Map<String, String> map = new HashMap<String, String>();
+		String dateString = ""; //开赛日期
+		String sxStartTime = ""; //上下半场开始时间
+		String[] matchTime2s = StringUtils.split(matchTime2, ",");
+		if (matchTime2s!=null&&matchTime2s.length==6) {
+			dateString = matchTime2s[0]+"-"+matchTime2s[1]+"-"+matchTime2s[2];
+			sxStartTime = matchTime2s[3]+":"+matchTime2s[4]+":"+matchTime2s[5];
+		}
+		//开赛时间
+		if (StringUtils.isNotBlank(dateString)) {
+			matchTime = dateString + " " + matchTime+":00";
+		} else {
+			matchTime = "";
+		}
+		//上下半场开始时间
+		if (StringUtils.isNotBlank(dateString)&&StringUtils.isNotBlank(sxStartTime)) {
+			matchTime2 = dateString + " " + sxStartTime;
+		} else {
+			matchTime2 = "";
+		}
+		map.put("matchTime", matchTime);
+		map.put("matchTime2", matchTime2);
+		return map;
+	}
+	
 }
