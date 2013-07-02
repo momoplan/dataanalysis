00148d2b7922e86f10b0eb9470cc558434526411
qujinlong <qujinlong@ruyicai.com>
2013-07-02 16:04:46 +0800

    findJcProcessingMatches():添加查询条件

--- src/main/java/com/ruyicai/dataanalysis/domain/Schedule.java
+++ src/main/java/com/ruyicai/dataanalysis/domain/Schedule.java
@@ -314,8 +314,8 @@ public class Schedule {
 	}
 	
 	public static List<Schedule> findJcProcessingMatches() {
-		List<Schedule> schedules = entityManager().createQuery("select o from Schedule o where event is not null and matchState in('1', '2', '3', '4') order by matchTime asc", Schedule.class)
-				.getResultList();
+		List<Schedule> schedules = entityManager().createQuery("select o from Schedule o where event is not null and matchState in('1', '2', '3', '4') AND SUBSTR(event,1,1)=? order by matchTime asc", Schedule.class)
+				.setParameter(1, "1").getResultList();
 		if(null != schedules) {
 			for(Schedule schedule : schedules) {
 				buildSchedule(schedule);
