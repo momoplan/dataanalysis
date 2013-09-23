148b3d8529f0d3f449ff9ab7f3f73b342aa8feec
qujinlong <qujinlong@ruyicai.com>
2013-09-23 09:46:32 +0800

    欧赔处理的Jms:将Standard,EuropeCompany是否存在放Map里

--- src/main/java/com/ruyicai/dataanalysis/listener/StandarJczUpdateListener.java
+++ src/main/java/com/ruyicai/dataanalysis/listener/StandarJczUpdateListener.java
@@ -1,29 +1,22 @@
 package com.ruyicai.dataanalysis.listener;
 
 import java.math.BigDecimal;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.LinkedList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import org.apache.camel.Body;
 import org.dom4j.Document;
 import org.dom4j.DocumentHelper;
 import org.dom4j.Element;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.ruyicai.dataanalysis.domain.EuropeCompany;
-import com.ruyicai.dataanalysis.domain.GlobalCache;
 import com.ruyicai.dataanalysis.domain.Schedule;
 import com.ruyicai.dataanalysis.domain.Standard;
 import com.ruyicai.dataanalysis.domain.StandardDetail;
-import com.ruyicai.dataanalysis.service.GlobalInfoService;
-import com.ruyicai.dataanalysis.util.CommonUtil;
 import com.ruyicai.dataanalysis.util.DateUtil;
-import com.ruyicai.dataanalysis.util.NumberUtil;
 import com.ruyicai.dataanalysis.util.StringUtil;
-import com.ruyicai.dataanalysis.util.jcz.CalcUtil;
 
 /**
  * 足球欧赔更新Jms的处理
@@ -34,6 +27,9 @@ import com.ruyicai.dataanalysis.util.jcz.CalcUtil;
 public class StandarJczUpdateListener {
 	
 	private Logger logger = LoggerFactory.getLogger(StandarJczUpdateListener.class);
+	
+	private Map<String, Boolean> companyMap = new HashMap<String, Boolean>();
+	private Map<String, Boolean> standardMap = new HashMap<String, Boolean>();
 	
 	/*@Autowired
 	private GlobalInfoService globalInfoService;*/
@@ -98,11 +94,16 @@ public class StandarJczUpdateListener {
 					t_g = t_g + new Double(firstGuestWin);
 				}
 				long startmillis2 = System.currentTimeMillis();
-				EuropeCompany company = EuropeCompany.findEuropeCompany(Integer.parseInt(companyID));
+				Boolean cHasExist = companyMap.get(companyID);
+				if (cHasExist==null||!cHasExist) {
+					EuropeCompany company = EuropeCompany.findEuropeCompany(Integer.parseInt(companyID));
+					cHasExist = company==null ? false : true;
+					companyMap.put(companyID, cHasExist);
+				}
 				long endmillis2 = System.currentTimeMillis();
 				logger.info("欧赔Jms获取EuropeCompany,用时"+(endmillis2-startmillis2));
-				if(null == company) {
-					company = new EuropeCompany();
+				if(!cHasExist) {
+					EuropeCompany company = new EuropeCompany();
 					company.setCompanyID(Integer.parseInt(companyID));
 					company.setName_Cn(companyName);
 					company.setName_E(companyName);
@@ -111,11 +112,17 @@ public class StandarJczUpdateListener {
 					company.persist();
 				}
 				long startmillis3 = System.currentTimeMillis();
-				Standard standard = Standard.findStandard(Integer.parseInt(scheduleID), Integer.parseInt(companyID));
+				String skey = StringUtil.join("_", scheduleID, companyID);
+				Boolean sHasExist = standardMap.get(skey);
+				if (sHasExist==null||!sHasExist) {
+					Standard standard = Standard.findStandard(Integer.parseInt(scheduleID), Integer.parseInt(companyID));
+					sHasExist = standard==null ? false : true;
+					standardMap.put(skey, sHasExist);
+				}
 				long endmillis3 = System.currentTimeMillis();
 				logger.info("欧赔Jms获取Standard,用时"+(endmillis3-startmillis3));
-				if(null == standard) {
-					standard = new Standard();
+				if(!sHasExist) {
+					Standard standard = new Standard();
 					standard.setScheduleID(Integer.parseInt(scheduleID));
 					standard.setCompanyID(Integer.parseInt(companyID));
 					standard.setFirstHomeWin(new Double(firstHomeWin));
