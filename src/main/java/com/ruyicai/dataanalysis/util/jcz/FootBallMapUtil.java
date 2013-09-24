a4b6c05cf619f1894e8bd5cffc3969a71f02af4a
qujinlong <qujinlong@ruyicai.com>
2013-09-24 14:03:04 +0800

    添加清空足球Map的定时任务

--- src/main/java/com/ruyicai/dataanalysis/util/jcz/FootBallMapUtil.java
+++ src/main/java/com/ruyicai/dataanalysis/util/jcz/FootBallMapUtil.java
@@ -2,11 +2,16 @@ package com.ruyicai.dataanalysis.util.jcz;
 
 import java.util.HashMap;
 import java.util.Map;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 @Service
 public class FootBallMapUtil {
 
+	private Logger logger = LoggerFactory.getLogger(FootBallMapUtil.class);
+	
 	public Map<String, Boolean> scheduleMap = new HashMap<String, Boolean>();
 	
 	public Map<String, Boolean> letgoalMap = new HashMap<String, Boolean>();
@@ -14,5 +19,18 @@ public class FootBallMapUtil {
 	public Map<String, Boolean> europeCompanyMap = new HashMap<String, Boolean>();
 	
 	public Map<String, Boolean> standardMap = new HashMap<String, Boolean>();
+	
+	public void clearMap() {
+		logger.info("清空足球Map开始");
+		try {
+			scheduleMap.clear();
+			letgoalMap.clear();
+			europeCompanyMap.clear();
+			standardMap.clear();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		logger.info("清空足球Map结束");
+	}
 	
 }
