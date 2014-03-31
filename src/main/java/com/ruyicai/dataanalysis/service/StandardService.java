a6df84d4cfc7a0f89a8a259bb4f20305ca1ceea2
qujinlong <qujinlong@ruyicai.com>
2014-03-31 09:12:01 +0800

    修改StandardService

--- src/main/java/com/ruyicai/dataanalysis/service/StandardService.java
+++ src/main/java/com/ruyicai/dataanalysis/service/StandardService.java
@@ -4,42 +4,44 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import org.apache.commons.lang.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import com.ruyicai.dataanalysis.cache.CacheService;
 import com.ruyicai.dataanalysis.domain.Schedule;
 import com.ruyicai.dataanalysis.domain.Standard;
 import com.ruyicai.dataanalysis.service.dto.StandardDto;
+import com.ruyicai.dataanalysis.util.StringUtil;
 
 @Service
 public class StandardService {
 
+	@Autowired
+	private CacheService cacheService;
+	
 	public Map<String, StandardDto> getUsualStandards(String companyId) {
-		Map<String, StandardDto> map = new HashMap<String, StandardDto>();
-		//查询betState=1在售的赛事
-		List<Integer> scheduleIds = Schedule.findSaleIds();
-		if (scheduleIds!=null&&scheduleIds.size()>0) {
-			for (Integer scheduleId : scheduleIds) {
-				Schedule schedule = Schedule.findSchedule(scheduleId);
-				if (StringUtils.isBlank(schedule.getEvent())) {
-					continue;
+		String key = StringUtil.join("_", "dadaanalysis", "UsualStandards", companyId);
+		Map<String, StandardDto> map = cacheService.get(key);
+		if (map==null) {
+			map = new HashMap<String, StandardDto>();
+			//查询betState=1在售的赛事
+			List<Integer> scheduleIds = Schedule.findSaleIds();
+			if (scheduleIds!=null&&scheduleIds.size()>0) {
+				for (Integer scheduleId : scheduleIds) {
+					Schedule schedule = Schedule.findSchedule(scheduleId);
+					if (StringUtils.isBlank(schedule.getEvent())) {
+						continue;
+					}
+					StandardDto dto = null;
+					if (StringUtils.equals(companyId, "avg")) { //平均欧赔
+						dto = getAvgStandardDto(schedule);
+					} else {
+						dto = getStandardDtoByCompanyId(scheduleId, Integer.parseInt(companyId));
+					}
+					map.put(schedule.getEvent(), dto);
 				}
-				StandardDto dto = null;
-				if (StringUtils.equals(companyId, "avg")) { //平均欧赔
-					dto = getAvgStandardDto(schedule);
-				} else {
-					dto = getStandardDtoByCompanyId(scheduleId, Integer.parseInt(companyId));
-				}
-				map.put(schedule.getEvent(), dto);
-				//威廉希尔
-				/*getStandardDtoByCompanyId(scheduleId, 115, list);
-				//立博
-				getStandardDtoByCompanyId(scheduleId, 82, list);
-				//bwin
-				getStandardDtoByCompanyId(scheduleId, 255, list);
-				//澳门
-				getStandardDtoByCompanyId(scheduleId, 80, list);
-				//bet365
-				getStandardDtoByCompanyId(scheduleId, 281, list);
-				*/
+			}
+			if (map.size()>0) {
+				cacheService.set(key, map);
 			}
 		}
 		return map;
@@ -47,7 +49,6 @@ public class StandardService {
 	
 	private StandardDto getAvgStandardDto(Schedule schedule) {
 		StandardDto avgDto = new StandardDto();
-		//avgDto.setCompanyId("avg");
 		avgDto.setHomeWin(schedule.getAvgH());
 		avgDto.setStandoff(schedule.getAvgS());
 		avgDto.setGuestWin(schedule.getAvgG());
@@ -60,7 +61,6 @@ public class StandardService {
 			return null;
 		}
 		StandardDto dto = new StandardDto();
-		//dto.setCompanyId(String.valueOf(companyId));
 		dto.setHomeWin(standard.getHomeWin());
 		dto.setStandoff(standard.getStandoff());
 		dto.setGuestWin(standard.getGuestWin());
