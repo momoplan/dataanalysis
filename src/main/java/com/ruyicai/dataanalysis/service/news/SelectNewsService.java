a29d1a792dfc790b3e999bb5f259715ce2ff94c5
qujinlong <qujinlong@ruyicai.com>
2014-05-26 17:11:19 +0800

    修改SelectNewsController

--- src/main/java/com/ruyicai/dataanalysis/service/news/SelectNewsService.java
+++ src/main/java/com/ruyicai/dataanalysis/service/news/SelectNewsService.java
@@ -2,14 +2,12 @@ package com.ruyicai.dataanalysis.service.news;
 
 import java.util.ArrayList;
 import java.util.List;
+import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import com.ruyicai.dataanalysis.consts.ErrorCode;
 import com.ruyicai.dataanalysis.dao.NewsDao;
 import com.ruyicai.dataanalysis.domain.news.News;
-import com.ruyicai.dataanalysis.exception.RuyicaiException;
 import com.ruyicai.dataanalysis.util.Page;
-import com.ruyicai.dataanalysis.util.StringUtil;
 
 @Service
 public class SelectNewsService {
@@ -18,14 +16,15 @@ public class SelectNewsService {
 	private NewsDao newsDao;
 	
 	public void findNews(String event, Page<News> page) {
-		if (StringUtil.isEmpty(event)) {
+		/*if (StringUtil.isEmpty(event)) {
 			throw new RuyicaiException(ErrorCode.PARAMTER_ERROR);
-		}
+		}*/
 		StringBuilder builder = new StringBuilder(" where");
 		List<Object> params = new ArrayList<Object>();
-		
-		builder.append(" o.event=? ");
-		params.add(event);
+		if (StringUtils.isNotBlank(event)) {
+			builder.append(" o.event=? ");
+			params.add(event);
+		}
 		newsDao.findListWithPage(builder.toString(), "order by o.publishtime desc", params, page);
 	}
 	
